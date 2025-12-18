b63df713cc360213652edf84dbe0278fb63ec64b
Ethan <ethanstark.mars@gmail.com>
2025-12-18 15:54:49 +0700

    fix: disable copy when delete

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "v" }, "d", '"_d', { silent = true })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { silent = true })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { silent = true })
+vim.keymap.set({ "n", "v" }, "Y", '"+y', { silent = true })
+

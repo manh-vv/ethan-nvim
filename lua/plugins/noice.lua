1649205ec746a7b50e396566946ddc37392c69bd
Ethan <manh.vv.htth@gmail.com>
2026-01-22 09:33:57 +0700

    feat: noice classic

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,12 @@ return {
   opts = {
     cmdline = {
       view = "cmdline", -- Changes the view to a classic cmdline at the bottom
+      format = {
+        cmdline = false,
+        search_down = false,
+        search_up = false,
+        lua = false,
+      },
     },
   },
 }
